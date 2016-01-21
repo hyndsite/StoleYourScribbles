7104a2ab80b9079de645431c0a69a323112d2687
McCarty <C000985@nac.ppg.com>
2016-01-21 16:02:40 -0500

    first commit

---
+++ StoleYourScribbles.Web/Controllers/ScribblesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace StoleYourScribbles.Web.Controllers
+{
+    [RoutePrefix("~/api/scribbles")]
+    public class ScribblesController : ApiController
+    {
+        [Route("~/api/scribbles/stolen")]
+        public void Stolen([FromUri] string data)
+        {
+            
+        }
+    }
+}
---
+++ StoleYourScribbles.Web/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoleYourScribbles.Web.WebApiApplication" Language="C#" %>
