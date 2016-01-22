54c1ea86f488baaae7156e15defadaa01514e1ae
McCarty <C000985@nac.ppg.com>
2016-01-22 14:47:55 -0500

    merge

---
+++ StoleYourScribbles.Web/Repositories/ScribblesRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using StoleYourScribbles.Web.Entities;
+
+namespace StoleYourScribbles.Web.Repositories
+{
+    public class ScribblesRepository
+    {
+        private readonly CloudTable _cloutTable;
+
+        public ScribblesRepository(CloudTable cloutTable)
+        {
+            _cloutTable = cloutTable;
+        }
+
+        public async Task<TableResult> SaveAsync(ScribbleEntity scribble)
+        {
+                var operation = TableOperation.Insert(scribble);
+                return await _cloutTable.ExecuteAsync(operation);
+        }
+    }
+
+}
---
+++ StoleYourScribbles.Web/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin.Cors;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Owin;
+
+namespace StoleYourScribbles.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+         var httpConfiguration = new HttpConfiguration();
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(httpConfiguration);
+
+            var task = InitializeStorage();
+            task.Wait();
+            httpConfiguration.MapHttpAttributeRoutes();
+        }
+
+        private async Task InitializeStorage()
+        {
+            var accountName = ConfigurationManager.AppSettings["AccountName"];
+            var accountKey = ConfigurationManager.AppSettings["AccountKey"];
+
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+
+            await ValidateTableStorage(storageAccount);
+        }
+
+        private async Task ValidateTableStorage(CloudStorageAccount storageAccount)
+        {
+            var tableName = ConfigurationManager.AppSettings["AzureTableName"];
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(tableName);
+
+            try
+            {
+                if (!await table.ExistsAsync())
+                {
+                    await table.CreateAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
